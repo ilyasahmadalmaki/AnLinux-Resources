367ff2a0055957f2b2122ba4d88a7d6ea02ea1bd
ilyasahmadalmaki <36147933+ilyasahmadalmaki@users.noreply.github.com>
2019-09-02 02:53:50 +0700

    Update ssh-apt.sh

--- Scripts/SSH/Apt/ssh-apt.sh
+++ Scripts/SSH/Apt/ssh-apt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#Get the necessary components
+#Install komponen necessary
 apt-get update
 apt-get install openssh-server -y
 
-#Setup the necessary files
+#Setup necessary file
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/sshd_config -P /etc/ssh
 
-echo "You can now start OpenSSH Server by running /etc/init.d/ssh start"
+echo "Anda sekarang dapat memulai OpenSSH Server dengan menjalankan /etc/init.d/ssh start"
 echo " "
-echo "The Open Server will be started at 127.0.0.1:22"
+echo "Server akan dimulai pada 127.0.0.1:22"
